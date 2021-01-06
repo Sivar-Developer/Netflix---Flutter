293ae5ae8e1f4700f85f0b9a63aff27138c91c3e
Sivar Sarkawt <fragmatica.dc@gmail.com>
2021-01-06 15:28:49 +0300

    Fade App Bar:

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -2,11 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_netflix_responsive_ui/assets.dart';
 
 class CustomAppBar extends StatelessWidget {
+  final double scrollOffset;
+
+  const CustomAppBar({
+    Key key,
+    this.scrollOffset = 0.0
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 24.0),
-      color: Colors.orange,
+      color: Colors.black.withOpacity((scrollOffset / 350).clamp(0, 1).toDouble()),
       child: SafeArea(
         child: Row(
           children: [
